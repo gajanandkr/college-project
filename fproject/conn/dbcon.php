4afe2bc1a831efb2402ab65170d52ddaa26f94b1
gajanandkr <35303319+gajanandkr@users.noreply.github.com>
2019-05-16 19:48:21 +0530

    s

--- fproject/conn/dbcon.php
+++
@@ -1,4 +0,0 @@
-<?php
-$cont= mysqli_connect("localhost","root","","college");
-
-?>
--- fproject/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
- session_start();
-  //if(isset($_SESSION['uname']))
- // {
-      session_destroy();
-      header("location:index.php");
-  //}
-?>
